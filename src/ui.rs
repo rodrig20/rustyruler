5941470a622b33f41657edec87e29153de99fcb4
rodrig20 <101592068+rodrig20@users.noreply.github.com>
2025-10-31 00:37:48 +0000

    Fix panicking closer to borders (#2)
    * fix: set minmum value to be 0
    
    * chore: Add upper value for consistency
    
    * fix: direction of crosshair lateral expansion

--- src/ui.rs
+++ src/ui.rs
@@ -135,48 +135,62 @@ fn draw_crosshair(cr: &cairo::Context, data: &CrosshairData, scale: f64) {
     cr.set_source_rgb(1.0, 0.0, 0.0);
     cr.set_line_width(1.0 / scale);
 
+    let lower_x = if data.x < 4 {
+        0
+    } else {
+        data.x - 4
+    };
+    let upper_x = data.x + 4;
+
     // Draw vertical line above the crosshair center
     cr.move_to(data.x as f64, data.y as f64);
     cr.line_to(data.x as f64, data.top_limit as f64);
     let _ = cr.stroke();
-    cr.move_to((data.x - 4) as f64, data.top_limit as f64);
-    cr.line_to((data.x + 4) as f64, data.top_limit as f64);
+    cr.move_to((lower_x) as f64, data.top_limit as f64);
+    cr.line_to((upper_x) as f64, data.top_limit as f64);
     let _ = cr.stroke();
-    cr.move_to((data.x - 4) as f64, (data.top_limit - 1) as f64);
-    cr.line_to((data.x + 4) as f64, (data.top_limit - 1) as f64);
+    cr.move_to((lower_x) as f64, (data.top_limit + 1) as f64);
+    cr.line_to((upper_x) as f64, (data.top_limit + 1) as f64);
     let _ = cr.stroke();
 
     // Draw vertical line below the crosshair center
     cr.move_to(data.x as f64, data.y as f64);
     cr.line_to(data.x as f64, data.bottom_limit as f64);
     let _ = cr.stroke();
-    cr.move_to((data.x - 4) as f64, data.bottom_limit as f64);
-    cr.line_to((data.x + 4) as f64, data.bottom_limit as f64);
+    cr.move_to((lower_x) as f64, data.bottom_limit as f64);
+    cr.line_to((upper_x) as f64, data.bottom_limit as f64);
     let _ = cr.stroke();
-    cr.move_to((data.x - 4) as f64, (data.bottom_limit - 1) as f64);
-    cr.line_to((data.x + 4) as f64, (data.bottom_limit - 1) as f64);
+    cr.move_to((lower_x) as f64, (data.bottom_limit - 1) as f64);
+    cr.line_to((upper_x) as f64, (data.bottom_limit - 1) as f64);
     let _ = cr.stroke();
+
+    let lower_y = if data.y < 4 {
+        0
+    } else {
+        data.y - 4
+    };
+    let upper_y = data.y + 4;
 
     // Draw horizontal line to the left of crosshair center
     cr.move_to(data.x as f64, data.y as f64);
     cr.line_to(data.left_limit as f64, data.y as f64);
     let _ = cr.stroke();
-    cr.move_to(data.left_limit as f64, (data.y - 4) as f64);
-    cr.line_to(data.left_limit as f64, (data.y + 4) as f64);
+    cr.move_to(data.left_limit as f64, (lower_y) as f64);
+    cr.line_to(data.left_limit as f64, (upper_y) as f64);
     let _ = cr.stroke();
-    cr.move_to((data.left_limit - 1) as f64, (data.y - 4) as f64);
-    cr.line_to((data.left_limit - 1) as f64, (data.y + 4) as f64);
+    cr.move_to((data.left_limit + 1) as f64, (lower_y) as f64);
+    cr.line_to((data.left_limit + 1) as f64, (upper_y) as f64);
     let _ = cr.stroke();
 
     // Draw horizontal line to the right of crosshair center
     cr.move_to(data.x as f64, data.y as f64);
     cr.line_to(data.right_limit as f64, data.y as f64);
     let _ = cr.stroke();
-    cr.move_to(data.right_limit as f64, (data.y - 4) as f64);
-    cr.line_to(data.right_limit as f64, (data.y + 4) as f64);
+    cr.move_to(data.right_limit as f64, (lower_y) as f64);
+    cr.line_to(data.right_limit as f64, (upper_y) as f64);
     let _ = cr.stroke();
-    cr.move_to((data.right_limit - 1) as f64, (data.y - 4) as f64);
-    cr.line_to((data.right_limit - 1) as f64, (data.y + 4) as f64);
+    cr.move_to((data.right_limit - 1) as f64, (lower_y) as f64);
+    cr.line_to((data.right_limit - 1) as f64, (upper_y) as f64);
     let _ = cr.stroke();
 
     // Draw center point of the crosshair
